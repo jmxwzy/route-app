9f833f21ed566115bf43e1fc7b802698600fc33a
jmxwzy <1140312750@qq.com>
2024-07-02 16:38:18 +0800

    导航栏组件 + web组件并路由

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import NavBar from './navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './home';
+import Linux from './linux';
+import Django from './django';
+import Web from './web';
 
 class App extends Component {
     state = {  } 
-    render() { 
-        return <h1>Hello World</h1>;
+    render() {
+        return (
+            <React.Fragment>
+                <NavBar></NavBar>
+                <div className="container">
+                    <Routes>
+                        <Route path='/' element={<Home></Home>}></Route>
+                        <Route path='/linux' element={<Linux></Linux>}></Route>
+                        <Route path='/django' element={<Django></Django>}></Route>
+                        <Route path='/web' element={<Web></Web>}></Route>
+                        <Route path='/web/content/:chapter' element={<Web></Web>}></Route>
+                    </Routes>
+                </div>
+                
+            </React.Fragment>
+        ); 
     }
 }
  
